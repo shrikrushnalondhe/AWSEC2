b67f66d744b06218ab7c0d233a338bd958dba0f7
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-17 09:39:03 +0530

    Update AWSEC2.tf

--- AWSEC2.tf
+++ AWSEC2.tf
@@ -72,11 +72,11 @@ output "aws_subnet_subnet_1" {
 resource "aws_instance" "terraform_wapp" {
     ami = "ami-02e136e904f3da870"
     instance_type = "t2.micro"
-   region = us-east-1
     vpc_security_group_ids =  [ "${aws_security_group.terraform_private_sg.id}" ]
     subnet_id = "${aws_subnet.terraform-subnet_1.id}"
     key_name               = "terraform-demo"
     count         = 1
+    region = us-east-1
     associate_public_ip_address = true
     tags = {
       Name              = "terraform_ec2_wapp_awsdev"
