64c9a66312e3a565858d5c25e1ea2a855b34a41c
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-17 12:05:45 +0530

    Update AWSEC2.tf

--- AWSEC2.tf
+++ AWSEC2.tf
@@ -78,7 +78,10 @@ resource "aws_instance" "terraform_wapp" {
     instance_type = "t2.micro"
     vpc_security_group_ids =  [ "${aws_security_group.terraform_private_sg.id}" ]
     subnet_id = "${aws_subnet.terraform-subnet_1.id}"
-    key_name               = "terraform-demo"
+    resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGWY8SDH0GnV4FhP3gPZozofXlfEgxGYpvHmZmJYnvnxMoafvH6y8/9uKMDfM+IJirurxG42gfwfL2Y70ETa0KtEjhEnjZkc09Tri++5WvgjEiiXkr2/pxLYKbDPxEGbKRiyFOFMquHXMxvIBrId3wIpa6mNaMv4fFfWlKXr+IMWGFX4/2RfoVbpkFg+Q6ijyRiUviXS8IjUdRixDrV44wFSwCTpi2v0FqPuTEdkrE1DXIBopseCVumQsCunWRqeU5wrzeZBWhzADvjjKHhb1N56mjgLSw0m6b/xrJNjvc8qAWmkazREW43220OqdkqJYb1lqvsaXism+wksmGv8pNroot@ip-172-31-26-94.ec2.internal"
+}
     count         = 1
     associate_public_ip_address = true
     tags = {
@@ -94,11 +97,6 @@ connection {
       private_key = file("/home/jenkins/keys/aws_key")
       timeout     = "4m"
    }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGWY8SDH0GnV4FhP3gPZozofXlfEgxGYpvHmZmJYnvnxMoafvH6y8/9uKMDfM+IJirurxG42gfwfL2Y70ETa0KtEjhEnjZkc09Tri++5WvgjEiiXkr2/pxLYKbDPxEGbKRiyFOFMquHXMxvIBrId3wIpa6mNaMv4fFfWlKXr+IMWGFX4/2RfoVbpkFg+Q6ijyRiUviXS8IjUdRixDrV44wFSwCTpi2v0FqPuTEdkrE1DXIBopseCVumQsCunWRqeU5wrzeZBWhzADvjjKHhb1N56mjgLSw0m6b/xrJNjvc8qAWmkazREW43220OqdkqJYb1lqvsaXism+wksmGv8pNroot@ip-172-31-26-94.ec2.internal"
 }
 
 output "instance_id_list"     { value = ["${aws_instance.terraform_wapp.*.id}"] }
