fe542892716372209bf1f2394137dd17bd3758cb
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-19 18:41:37 +0530

    Update AWSEC2.tf

--- AWSEC2.tf
+++ AWSEC2.tf
@@ -69,6 +69,7 @@ output "aws_security_gr_id" {
 resource "aws_subnet" "subnet_dev" {
   vpc_id     = "${aws_vpc.terraform-vpc.id}"
   cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
 
   tags = {
@@ -82,6 +83,7 @@ output "aws_subnet_subnet_dev" {
 resource "aws_subnet" "subnet_prod" {
   vpc_id     = "${aws_vpc.terraform-vpc.id}"
   cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1b"
 
   tags = {
