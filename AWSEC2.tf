4096f01c8a95b73d2c25e0f396733db78ad59d01
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-20 10:43:59 +0530

    Update AWSEC2.tf

--- AWSEC2.tf
+++ AWSEC2.tf
@@ -17,7 +17,7 @@ output "aws_vpc_id" {
 
 ## Security Group##
 resource "aws_security_group" "terraform_private_sg" {
- ##description = "Allow limited inbound external traffic"##
+ description = "Allow inbound external traffic"
   vpc_id      = "${aws_vpc.terraform-vpc.id}"
   name        = "terraform_ec2_private_sg"
 
