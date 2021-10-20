b1fe582b33644284cedf60975775e55a2299756f
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-20 10:53:04 +0530

    Update AWSEC2.tf

--- AWSEC2.tf
+++ AWSEC2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 ## Create VPC ##
 resource "aws_vpc" "terraform-vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.0.5.0/16"
   enable_dns_hostnames = true
   tags = {
     Name = "product-demo-vpc"
